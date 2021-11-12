583b6d85608d7761e64089cffb090c7ee9c0bdcf
Roberto Marsella <roberto.marsella@icloud.com>
2021-11-12 19:29:35 +0100

    lavoro sulle pagine di log in

--- JongoApplicazione/JongoApplicazione/JongoApplicazione/PagineLogIn/LogInPage.xaml.cs
+++ JongoApplicazione/JongoApplicazione/JongoApplicazione/PagineLogIn/LogInPage.xaml.cs
@@ -26,8 +26,28 @@ namespace JongoApplicazione
         {
             if (Email.Text != null && Password.Text != null)
             {
-                Console.WriteLine("stampa: " + Email.Text);
+                Console.WriteLine("stampa: " + Email.Text + Password.Text);
                 Navigation.PushAsync(new MainPage());
+            }
+            else
+            {
+                if(Email.Text == null)
+                {
+                    Errore_Email.IsEnabled = true;
+                }
+                else
+                {
+                    Errore_Email.IsEnabled = false;
+                }
+
+                if(Password.Text == null)
+                {
+                    Errore_Password.IsEnabled = true;
+                }
+                else
+                {
+                    Errore_Password.IsEnabled = false;
+                }
             }
                 
         }
