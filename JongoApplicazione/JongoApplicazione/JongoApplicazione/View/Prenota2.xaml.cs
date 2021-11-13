af631b566b5cafb9e76e36c18bc86cc28649397f
FabLetizia <fabio.letizia01@gmail.com>
2021-11-13 17:47:22 +0100

    creazione pagine prenotazione

---
+++ JongoApplicazione/JongoApplicazione/JongoApplicazione/View/Prenota2.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace JongoApplicazione.View
+{
+    public partial class Prenota2 : ContentView
+    {
+        public Prenota2()
+        {
+            InitializeComponent();
+        }
+    }
+}
