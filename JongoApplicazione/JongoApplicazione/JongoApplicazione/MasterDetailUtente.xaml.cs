7c9bda7aba46c31fd8338572ee520a0c981661ef
Roberto Marsella <roberto.marsella@icloud.com>
2021-11-15 15:51:10 +0100

    creazione pagina per menu

---
+++ JongoApplicazione/JongoApplicazione/JongoApplicazione/MasterDetailUtente.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace JongoApplicazione
+{
+    public partial class MasterDetailUtente : MasterDetailPage
+    {
+        public MasterDetailUtente()
+        {
+            InitializeComponent();
+        }
+    }
+}
