758c44be7f9a1761f637e753609ac95e22d420e7
Roberto Marsella <roberto.marsella@icloud.com>
2022-01-02 18:22:36 +0100

    aggiunte nel progetto icone ancora non funzionanti

--- JongoApplicazione/JongoApplicazione/JongoApplicazione.Android/Resources/Resource.designer.cs
+++ JongoApplicazione/JongoApplicazione/JongoApplicazione.Android/Resources/Resource.designer.cs
@@ -11793,6 +11793,9 @@ namespace JongoApplicazione.Droid
 			// aapt resource value: 0x7F070091
 			public const int tooltip_frame_light = 2131165329;
 			
+			// aapt resource value: 0x7F070092
+			public const int User = 2131165330;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
