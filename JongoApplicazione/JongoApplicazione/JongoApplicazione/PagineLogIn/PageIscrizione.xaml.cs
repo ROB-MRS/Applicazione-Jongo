8d856333d3ed45e6f7acdd546fc391fdd34e4911
ADP2000 <ant.dellaporta1@stud.uniroma3.it>
2022-01-12 21:11:16 +0100

    connessione database

--- JongoApplicazione/JongoApplicazione/JongoApplicazione/PagineLogIn/PageIscrizione.xaml.cs
+++ JongoApplicazione/JongoApplicazione/JongoApplicazione/PagineLogIn/PageIscrizione.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 
+using System.Data.SqlClient;
 using Xamarin.Forms;
+
 
 namespace JongoApplicazione.PagineLogIn
 {
@@ -14,6 +16,22 @@ namespace JongoApplicazione.PagineLogIn
 
         void Bottone_premuto(System.Object sender, System.EventArgs e)
         {
+            try {
+                string srvrdbname = "Jongo";
+                string srvrname = "192.168.1.254";
+                string srvrusername = "adp";
+                string srvrpasswd = "Anto4700";
+                string sqlconn = $"Data Source = {srvrname};Initial Catalog = {srvrdbname};User Id = {srvrusername};Password = {srvrpasswd}; Trudted_connection = true";
+
+                SqlConnection connessione = new SqlConnection(sqlconn);
+                connessione.Open();
+            }
+                
+            catch (Exception ex){
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+            
             Etichetta.IsVisible = true;   
         }
 
