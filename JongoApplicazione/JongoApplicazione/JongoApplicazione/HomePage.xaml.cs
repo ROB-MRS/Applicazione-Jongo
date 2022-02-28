890b4509f3f03e0e0a5fa5a43e5f447953be13f0
Roberto Marsella <roberto.marsella@icloud.com>
2022-02-28 12:29:02 +0100

    prova

--- JongoApplicazione/JongoApplicazione/JongoApplicazione/HomePage.xaml.cs
+++ JongoApplicazione/JongoApplicazione/JongoApplicazione/HomePage.xaml.cs
@@ -12,6 +12,11 @@ namespace JongoApplicazione
             InitializeComponent();
         }
 
+        void Button_1_Clicked(System.Object sender, System.EventArgs e)
+        {
+            
+        }
+
         void Button_Clicked(System.Object sender, System.EventArgs e)
         {
             Navigation.PushAsync(new NavigationPage(new PagineLogIn.PageIscrizione()));
