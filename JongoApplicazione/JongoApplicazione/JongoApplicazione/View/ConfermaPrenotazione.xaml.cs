8f668bef48571861b50210dc7ac2066807e233c7
FabLetizia <fabio.letizia01@gmail.com>
2022-03-02 17:14:46 +0100

    Pagina di conferma prenotazione

---
+++ JongoApplicazione/JongoApplicazione/JongoApplicazione/View/ConfermaPrenotazione.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace JongoApplicazione.View
+{
+    public partial class ConfermaPrenotazione : ContentPage
+    {
+        public ConfermaPrenotazione()
+        {
+            InitializeComponent();
+        }
+    }
+}
