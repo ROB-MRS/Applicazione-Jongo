f110f7671229ed13928bd422ac4fd8c6ee1549dc
FabLetizia <fabio.letizia01@gmail.com>
2022-03-02 17:15:19 +0100

    rifatta seconda pag prenotazione

---
+++ JongoApplicazione/JongoApplicazione/JongoApplicazione/PagePrenotazione/Page2.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace JongoApplicazione.JongoApplicazione.PagePrenotazione
+{
+    public partial class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        //Bottone AVANTI
+         void Bottone_Avanti_Cliccato(System.Object sender, System.EventArgs e)
+        {
+           
+
+        }
+
+        //Bottone Indietro
+         void Bottone_Indietro_Cliccato(System.Object sender, System.EventArgs e)
+        {
+                Navigation.PushAsync(new NavigationPage(new PagePrenotazione.Page1()));
+
+        }
+    }
+}
