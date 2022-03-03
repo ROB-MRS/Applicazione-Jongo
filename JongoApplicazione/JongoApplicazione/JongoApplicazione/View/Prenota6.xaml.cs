445a1b07402e96c2d0791a752cc005af0bae3913
FabLetizia <fabio.letizia01@gmail.com>
2022-03-03 19:43:00 +0100

    Creata page Prenota6

--- JongoApplicazione/JongoApplicazione/JongoApplicazione/View/Prenota6.xaml.cs
+++ JongoApplicazione/JongoApplicazione/JongoApplicazione/View/Prenota6.xaml.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using Xamarin.Forms;
 
-namespace JongoApplicazione.JongoApplicazione.View
+namespace JongoApplicazione.View
 {
     public partial class Prenota6 : ContentView
     {
         public Prenota6()
         {
             InitializeComponent();
+        }
+        void Button_OK_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new NavigationPage(new HomePage()));
+
         }
     }
 }
