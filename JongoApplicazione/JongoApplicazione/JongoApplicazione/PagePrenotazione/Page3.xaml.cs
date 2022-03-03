e43d0b33dc5f204a8f2e532a4dfca1e2cc681764
FabLetizia <fabio.letizia01@gmail.com>
2022-03-03 19:03:52 +0100

    Page3 Prenotazione

---
+++ JongoApplicazione/JongoApplicazione/JongoApplicazione/PagePrenotazione/Page3.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace JongoApplicazione.JongoApplicazione.PagePrenotazione
+{
+    public partial class Page3 : ContentPage
+    {
+        public Page3()
+        {
+            InitializeComponent();
+        }
+    }
+}
