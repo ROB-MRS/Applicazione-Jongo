c280ba0831e9b8505e3c1dd56faca84a34c15a36
ADP2000 <ant.dellaporta1@stud.uniroma3.it>
2022-03-23 16:24:26 +0100

    cambia password in impostazioni

--- JongoApplicazione/JongoApplicazione/JongoApplicazione/Cronologia.xaml.cs
+++ JongoApplicazione/JongoApplicazione/JongoApplicazione/Cronologia.xaml.cs
@@ -18,98 +18,107 @@ namespace JongoApplicazione.JongoApplicazione
         public Cronologia(Utente utente)
         {
             InitializeComponent();
-            StackLayout stackLayout = new StackLayout();
-            foreach(Prenotazione p in utente.prenotazioni)
+            if (utente.prenotazioni.Count == 0) 
+            { 
+                Vuota.IsVisible = true;
+            }
+            else
             {
-                Frame frame = new Frame
+                StackLayout stackLayout = new StackLayout();
+                foreach (Prenotazione p in utente.prenotazioni)
                 {
-                    WidthRequest = 300,
-                    HeightRequest = 300,
-                    BackgroundColor = Color.LightGoldenrodYellow,
-                    BorderColor = Color.Black,
-                    Margin = new Thickness(10),
-                    CornerRadius = 20,
-                };
+                    Frame frame = new Frame
+                    {
+                        WidthRequest = 300,
+                        HeightRequest = 220,
+                        BackgroundColor = Color.Goldenrod,
+                        BorderColor = Color.Black,
+                        Margin = new Thickness(10),
+                        CornerRadius = 20,
+                    };
 
-                StackLayout stackLayout1= new StackLayout
-                {
-                    FlowDirection = FlowDirection.LeftToRight,
-                };
+                    StackLayout stackLayout1 = new StackLayout
+                    {
+                        FlowDirection = FlowDirection.LeftToRight,
+                    };
 
-                Label label = new Label
-                {
-                    Text = p.name + " " + p.cognome,
-                    Margin = new Thickness(0.8),
-                    FontSize = 50,
-                    TextColor = Color.Black,
-                };
+                    Label label = new Label
+                    {
+                        Text = p.name + " " + p.cognome,
+                        Margin = new Thickness(0.8),
+                        FontSize = 30,
+                        TextColor = Color.Black,
+                    };
 
-                Label label1 = new Label
-                {
-                    Text = "Servizio prenotato: " + p.servizio,
-                    Margin = new Thickness(0.4),
-                    FontSize = 20,
-                    TextColor = Color.Black,
-                };
+                    Label label1 = new Label
+                    {
+                        Text = "Servizio prenotato: " + p.servizio,
+                        Margin = new Thickness(0.4),
+                        FontSize = 20,
+                        TextColor = Color.Black,
+                    };
 
-                Label label2 = new Label
-                {
-                    Text = "Descrizione del servizio: " + p.descrizione,
-                    Margin = new Thickness(0.4),
-                    FontSize = 20,
-                    TextColor = Color.Black,
-                };
+                    Label label2 = new Label
+                    {
+                        Text = "Descrizione del servizio: " + p.descrizione,
+                        Margin = new Thickness(0.4),
+                        FontSize = 20,
+                        TextColor = Color.Black,
+                    };
 
-                Label label3 = new Label
-                {
-                    Text = "&#x2022; " + p.data + " " + p.ora,
-                    Margin = new Thickness(0.4),
-                    FontSize = 20,
-                    TextColor = Color.Black,
-                };
+                    Label label3 = new Label
+                    {
+                        Text = "Giorno: " + p.data,
+                        Margin = new Thickness(0.4),
+                        FontSize = 20,
+                        TextColor = Color.Black,
+                    };
 
+                    Label label4 = new Label
+                    {
+                        Text = "Ora: " + p.ora,
+                        Margin = new Thickness(0.4),
+                        FontSize = 20,
+                        TextColor = Color.Black,
+                    };
 
+                    Label label5 = new Label
+                    {
+                        Text = "Indirizzo: " + p.via + ", " + p.cap,
+                        Margin = new Thickness(0.4),
+                        FontSize = 20,
+                        TextColor = Color.Black,
+                    };
 
-                Button button = new Button
-                {
-                    Text = "Infomazioni Aggiuntive",
-                    CornerRadius = 10,
-                    FontSize = 50,
-                    BackgroundColor = Color.Violet,
-                    TextColor = Color.Black,
+                    //Button button = new Button
+                    //{
+                    //    Text = "Infomazioni Aggiuntive",
+                    //    Margin = new Thickness(10),
+                    //    CornerRadius = 10,
+                    //    FontSize = 20,
+                    //    BackgroundColor = Color.Violet,
+                    //    TextColor = Color.Black,
+                    //};
+                    //button.Clicked += async (sender, e) => await Navigation.PopAsync();
 
-                };
-                stackLayout1.Children.Add(label);
-                stackLayout1.Children.Add(label1);
-                stackLayout1.Children.Add(label2);
-                stackLayout1.Children.Add(label3);
-                stackLayout1.Children.Add(button);
-                frame.Content = stackLayout1;
-                //Label label = new Label
-                //{
-                //    Text = p.name+" "+p.cognome,
-                //    TextColor = Color.Black,
-                //    BackgroundColor = Color.Purple,
-                //    FontSize = 24,
-                //    VerticalOptions = LayoutOptions.Center
-                //};
-                //stackLayout.Children.Add(label);
-                //stackLayout.Margin = new Thickness(20,20);
+                    stackLayout1.Children.Add(label);
+                    stackLayout1.Children.Add(label1);
+                    stackLayout1.Children.Add(label2);
+                    stackLayout1.Children.Add(label3);
+                    stackLayout1.Children.Add(label4);
+                    stackLayout1.Children.Add(label5);
+                    //stackLayout1.Children.Add(button);
+                    frame.Content = stackLayout1;
+                    
+                    stackLayout.Children.Add(frame);
 
-                //StackLayout stackLayout = new StackLayout
-                //{
-                //    Margin = new Thickness(20,20), 
-                //    HeightRequest = 50,
-                //    HorizontalOptions = LayoutOptions.Start,
-                //    VerticalOptions= LayoutOptions.Start,
-                //    Orientation = StackOrientation.Horizontal,
-                //    Children = { label }
-                //};
-                stackLayout.Children.Add(frame);
+                }
+                pagina.Content = stackLayout;
 
             }
-            pagina.Content = stackLayout;
-            
+
         }
+
+        
     }
 }
