53c68d94eda169e73d0cf8b708a442a56d0037d2
ADP2000 <ant.dellaporta1@stud.uniroma3.it>
2022-05-19 17:49:14 +0200

    Messo background color static

--- JongoApplicazione/JongoApplicazione/JongoApplicazione/PagineLogIn/LogInPage.xaml.cs
+++ JongoApplicazione/JongoApplicazione/JongoApplicazione/PagineLogIn/LogInPage.xaml.cs
@@ -35,65 +35,7 @@ namespace JongoApplicazione
         //Bottone di Log In
         async void Bottone_Log_In_Cliccato(System.Object sender, System.EventArgs e)
         {
-            /*if (!String.IsNullOrEmpty(Email.Text) && !String.IsNullOrEmpty(Password.Text))
-            {
-                Errore_Password.IsVisible = false;
-                Console.WriteLine("stampa: " + Email.Text + Password.Text);
-                Navigation.PushAsync(new MainPage());
-            }
-            else
-            {
-                if(String.IsNullOrEmpty(Email.Text))
-                {
-                    Errore_Email.IsVisible = true;
-                }
-                else
-                {
-                    Errore_Email.IsVisible = false;
-                }
-
-                if(String.IsNullOrEmpty(Password.Text))
-                {
-                    Errore_Password.IsVisible = true;
-                }
-                else
-                {
-                    Errore_Password.IsVisible = false;
-                }
-            }
-            */
-
-            /*List<Utente> listaUtenti = new List<Utente>(await repository.GetAll());
-            Utente ut = null;
-            foreach (Utente utente in listaUtenti)
-            {
-                if(utente.Email == Email.Text)
-                {
-                    ut = utente;
-                }
-            }
-
-            if(ut == null)
-            {
-                await DisplayAlert("Errore", "Email errata", "OK");
-            }
-
-            else
-            {
-                if(ut.Password == Password.Text.GetHashCode())
-                {
-                    descrizioneUtente = ut.Name + " " + ut.Surname;
-                    Errore_Password.IsVisible = false;
-                    Console.WriteLine("stampa: " + Email.Text + Password.Text);
-                    await Navigation.PushAsync(new HomePage(descrizioneUtente));
-                }
-
-                else
-                {
-                    await DisplayAlert("Errore", "Password errata", "OK");
-                }
-            }*/
-
+            
             if (string.IsNullOrEmpty(Email.Text))
             {
                 await DisplayAlert("Errore", "Inserisci l'email", "OK");
