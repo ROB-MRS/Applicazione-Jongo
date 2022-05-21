bc6f25e33774fc5a0de4cec1c6a6751cdd39e763
Roberto Marsella <roberto.marsella@icloud.com>
2022-05-21 18:09:22 +0200

    prova ricarica aggiornamenti mail

--- JongoApplicazione/JongoApplicazione/ViewModel/PrenotazioniVM.cs
+++ JongoApplicazione/JongoApplicazione/ViewModel/PrenotazioniVM.cs
@@ -108,8 +108,8 @@ namespace JongoApplicazione.ViewModel
                     content = Prenota6;
                     string dest = "";
                     if (string.IsNullOrEmpty(p.mail)) { dest = u.Email; } else { dest = p.mail; }
-                    CreateMail("info.jongo@gmail.com", "PRENOTAZIONE LAVORO n'{this.p.id.ToString()}", CreateMessage(true));
-                    CreateMail(dest, "RIEPILOGO ORDINE JONGO n'{this.p.id.ToString()} " , CreateMessage(false));
+                    CreateMail("info.jongo@gmail.com", $"PRENOTAZIONE LAVORO n'{this.p.id.ToString()}", CreateMessage(false));
+                    CreateMail(dest, $"RIEPILOGO ORDINE JONGO n'{this.p.id.ToString()} " , CreateMessage(true));
                     AggiornaPagina();
                     return;
                 default:
@@ -158,11 +158,20 @@ namespace JongoApplicazione.ViewModel
             if (valore)
             {
                 messaggio.Append("Gentile cliente, ecco un riepilogo dell'ordine effettuato tramite il servizio JONGO:")
-                .Append($"Nome: {i.nome}\n").Append($"Cognome: {i.cognome}/n")
+                .Append($"Nome: {i.nome}\n").Append($"Cognome: {i.cognome}\n").Append($"Telefono/Cellulare: {i.cell}\n")
+                .Append($"Presso: {i.via}").Append($",{i.cap}\n").Append($"In data: {i.data}").Append($"alle ore {i.data}\n")
+                .Append($"Per il servizio di: {i.servizio}\n").Append($"Descrizione: {i.descrizione}\n").Append($"[info extra: {i.infoExtra}]")
+                .Append("Ti ringraziamo per aver utilizzato Jongo. Presto verrai contattato da un nostro operatore per un preventivo accurato.\n" +
+                        "Per altre informazioni puoi contattarci a info@jongomontaggi.it o al numero verdi +39 351 7085953.\n\n");
                 return messaggio.ToString();
             }
             else
             {
+                messaggio.Append("Richesta di lavoro Jongo:")
+                .Append($"Nome: {i.nome}\n").Append($"Cognome: {i.cognome}\n").Append($"Telefono/Cellulare: {i.cell}\n")
+                .Append($"Presso: {i.via}").Append($",{i.cap}\n").Append($"In data: {i.data}").Append($"alle ore {i.data}\n")
+                .Append($"Per il servizio di: {i.servizio}\n").Append($"Descrizione: {i.descrizione}\n").Append($"[info extra: {i.infoExtra}]")
+                .Append("\n\nInviare al piu presto il preventivo al cliente.");
                 return messaggio.ToString();
             }
         }
